c386af1e8acb61e14c283911e59747ea27e0c75b
Wayne <wayhap@gmail.com>
2014-06-19 17:12:17 +0800

    more fit

--- WAYNE_IOS_SDK/文字处理/DateEx.m
+++ WAYNE_IOS_SDK/文字处理/DateEx.m
@@ -7,10 +7,12 @@
 //
 
 #import "DateEx.h"
-//@"2019-10-15T09:46:16+08:00"
+//@"2019-10-15T09:46:16+08:00"  (19)+08:00
 static NSString * const FormatString = @"yyyy-MM-dd'T'HH:mm:ssZZZZZ";
-//@"2019-10-15T09:46:16.000"
-static NSString * const FormatString2 = @"yyyy-MM-dd'T'HH:mm:ss.SSS";
+//@"2019-10-15T09:46:16.000"  (19).xxx
+static NSString * const FormatStringPoint = @"yyyy-MM-dd'T'HH:mm:ss.SSS";
+//@"2019-10-15T09:46:16"  (19)
+static NSString * const FormatString19 = @"yyyy-MM-dd'T'HH:mm:ss";
 @implementation NSDate(WEX)
 
 -(NSString*)iso8601Format{
@@ -18,16 +20,30 @@ static NSString * const FormatString2 = @"yyyy-MM-dd'T'HH:mm:ss.SSS";
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     NSLocale *locale = [NSLocale localeWithLocaleIdentifier:@"zh_Hans_CN"];
     [dateFormatter setLocale:locale];
-    [dateFormatter setDateFormat:FormatString2];
+    [dateFormatter setDateFormat:FormatString];
     NSString *iso8601String = [dateFormatter stringFromDate:self];
     return iso8601String;
 }
 +(id)dateWithISO8601String:(NSString*)str{
+    if (str.length<19) {
+        return @"plz use iso8601 format string";
+    }
     NSDateFormatter *dateFormatter = [NSDateFormatter new];
     NSLocale *locale = [[NSLocale alloc]initWithLocaleIdentifier:@"zh_Hans_CN"];
     [dateFormatter setLocale:locale];
 //    [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
-    [dateFormatter setDateFormat:FormatString2];
+    NSString * formatString;
+    if (str.length==19) {
+        formatString = FormatString19;
+    }else{
+        NSString *str20 = [str substringWithRange:NSMakeRange(19, 1)];
+        if ([str20 isEqual:@"+"]) {
+            formatString = FormatString;
+        }else{
+            formatString = FormatStringPoint;
+        }
+    }
+    [dateFormatter setDateFormat:formatString];
     NSDate *date = [dateFormatter dateFromString:str];
     return date;
 }
